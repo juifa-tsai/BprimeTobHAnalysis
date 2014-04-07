90f3846c01eac5ca99316c2762fac1bc62f3a079
Devdatta <devdatta.majumder@cern.ch>
2014-04-07 19:00:54 +0200

    compareBJets.C

--- macros/compareBJets.C
+++ macros/compareBJets.C
@@ -3,9 +3,9 @@
 
 void compare (TString& hist) {
 
-  TFile* f0 = TFile::Open("/afs/cern.ch/work/d/devdatta/CMSREL/CMSSW_5_3_13_patch3_Bpbh/src/BpbH/BprimeTobHAnalysis/test/bprimeTobHSkimmed_BpBp500_bHiggsOverlapRemoved.root","READ") ; 
-  TFile* f1 = TFile::Open("/afs/cern.ch/work/d/devdatta/CMSREL/CMSSW_5_3_13_patch3_Bpbh/src/BpbH/BprimeTobHAnalysis/test/bprimeTobHSkimmed_BpBp500_bHiggsHighMassOverlapRemoved.root","READ") ; 
-  TFile* f2 = TFile::Open("/afs/cern.ch/work/d/devdatta/CMSREL/CMSSW_5_3_13_patch3_Bpbh/src/BpbH/BprimeTobHAnalysis/test/bprimeTobHSkimmed_BpBp500_bHiggsAntiHiggsOverlapRemoved.root","READ") ; 
+  TFile* f0 = TFile::Open("/afs/cern.ch/work/d/devdatta/CMSREL/CMSSW_5_3_13_patch3_Bpbh/src/BpbH/BprimeTobHAnalysis/test/bprimeTobHSkimmed_BpBp1000_bHiggsOverlapRemoved.root","READ") ; 
+  TFile* f1 = TFile::Open("/afs/cern.ch/work/d/devdatta/CMSREL/CMSSW_5_3_13_patch3_Bpbh/src/BpbH/BprimeTobHAnalysis/test/bprimeTobHSkimmed_BpBp1000_bHiggsHighMassOverlapRemoved.root","READ") ; 
+  TFile* f2 = TFile::Open("/afs/cern.ch/work/d/devdatta/CMSREL/CMSSW_5_3_13_patch3_Bpbh/src/BpbH/BprimeTobHAnalysis/test/bprimeTobHSkimmed_BpBp1000_bHiggsAntiHiggsOverlapRemoved.root","READ") ; 
 
   TH1D* h0 = (TH1D*)f0->Get("BprimebH/"+hist) ; 
   TH1D* h1 = (TH1D*)f1->Get("BprimebH/"+hist) ; 
@@ -24,19 +24,19 @@ void compare (TString& hist) {
 
   h0->SetMaximum(std::max(std::max(h0->GetMaximum(), h1->GetMaximum()), h2->GetMaximum())*1.5) ; 
 
-  TCanvas* c500 = new TCanvas("c500"+hist, "c500"+hist, 800, 600) ;
-  c500->cd();
+  TCanvas* c1000 = new TCanvas("c1000"+hist, "c1000"+hist, 800, 600) ;
+  c1000->cd();
   h0->Draw("HIST") ;
-  c500->Modified() ; c500->Update(); 
-  TPaveStats *st0 = (TPaveStats*)c500->GetPrimitive("stats") ; 
+  c1000->Modified() ; c1000->Update(); 
+  TPaveStats *st0 = (TPaveStats*)c1000->GetPrimitive("stats") ; 
   st0->SetName("st0") ; 
   h1->Draw("HISTSAMES") ;
-  c500->Modified() ; c500->Update(); 
-  TPaveStats *st1 = (TPaveStats*)c500->GetPrimitive("stats") ; 
+  c1000->Modified() ; c1000->Update(); 
+  TPaveStats *st1 = (TPaveStats*)c1000->GetPrimitive("stats") ; 
   st1->SetName("st1") ; 
   h2->Draw("HISTSAMES") ;
-  c500->Modified() ; c500->Update(); 
-  TPaveStats *st2 = (TPaveStats*)c500->GetPrimitive("stats") ; 
+  c1000->Modified() ; c1000->Update(); 
+  TPaveStats *st2 = (TPaveStats*)c1000->GetPrimitive("stats") ; 
   st2->SetName("st2") ; 
 
   st1->SetY2NDC(st0->GetY1NDC()) ; 
@@ -47,7 +47,7 @@ void compare (TString& hist) {
   st1->SetTextColor(kBlue) ; 
   st2->SetTextColor(kGreen) ; 
 
-  TLegend * leg = new TLegend(0.12,0.75,0.78,0.88,"M(b') = 500 GeV","brNDC") ; 
+  TLegend * leg = new TLegend(0.12,0.75,0.78,0.88,"M(b') = 1000 GeV","brNDC") ; 
   leg->SetBorderSize(0) ;
   leg->SetFillColor(0);
   leg->AddEntry(h0, hist+": No H M(H) = [0,140] GeV overlap", "l") ;
@@ -55,7 +55,8 @@ void compare (TString& hist) {
   leg->AddEntry(h2, hist+": No H M(H) = [0,75] GeV or anti-H overlap", "l") ;
   leg->Draw() ; 
 
-  c500->SaveAs(c500->GetName()+TString(".png")) ; 
+  c1000->SaveAs(TString("Overlap_H_AntiH_BJets/")+c1000->GetName()+TString(".png")) ; 
+  c1000->SaveAs(TString("Overlap_H_AntiH_BJets/")+c1000->GetName()+TString(".pdf")) ; 
 
 }
 
